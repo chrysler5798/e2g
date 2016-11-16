b204ba20119369f417495d9acb27a0dcccf48538
chrysler5798 <chrysler5798@hotmail.fr>
2016-11-16 18:06:26 +0100

    Initial commit

---
+++ app/src/main/java/com/event_to_give/e2g/MainActivity.java
@@ -0,0 +1,185 @@
+package com.event_to_give.e2g;
+
+import android.graphics.Bitmap;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+
+
+public class MainActivity extends AppCompatActivity
+{
+
+    WebView firstWeb;
+
+    final String FACEBOOK_URL = "https://www.facebook.com/assoc.eventtogive";
+    final String TWITTER_URL = "https://twitter.com/Event2Give";
+
+    private String[] mMenuTitles;
+    private ListView mDrawerList;
+    private DrawerLayout mDrawerLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+
+        myToolbar.setTitle("Event To Give");
+        myToolbar.setNavigationIcon(R.drawable.ic_list_black_24dp);
+        setSupportActionBar(myToolbar);
+
+        mMenuTitles = getResources().getStringArray(R.array.menu_arrays);
+        mDrawerList = (ListView) findViewById(R.id.left_drawer);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerLayout.setDrawerShadow(R.drawable.drawer_shadow, GravityCompat.START);
+
+        mDrawerList.setAdapter(new ArrayAdapter<>(this, R.layout.drawer_list_item, mMenuTitles));
+        mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
+        firstWeb = (WebView) findViewById(R.id.webView1);
+
+        firstWeb.getSettings().setJavaScriptEnabled(true);
+
+        firstWeb.setWebViewClient(new WebViewClient()
+        {
+            @Override
+            public void onPageCommitVisible(WebView view, String url) {
+                findViewById(R.id.loadingPanel).setVisibility(View.GONE);
+                firstWeb.loadUrl("javascript:(function() { " + "document.getElementsByClassName('container clearfix')[0].style.display = 'none'; " + "})()");
+                //firstWeb.loadUrl("javascript:(function() { " + "document.getElementsByClassName('mainnav-toggle')[0].style.display = 'none'; " + "})()");
+            }
+
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                findViewById(R.id.loadingPanel).setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                firstWeb.loadUrl("javascript:(function() { " + "document.getElementsByClassName('mainnav-toggle')[0].style.display = 'none'; " + "})()");
+            }
+
+            @Override
+            public void onReceivedError(WebView view, int errorCode, String description, String failingUrl)
+            {
+                if (errorCode == -2)
+                {
+                    //String summary = "<html><body style='background: black;'><p style='color: red;'>Unable to load information. Please check if your network connection is working properly or try again later.</p></body></html>";
+                    //view.loadData(summary, "text/html", null);
+                    //Todo: Add an errorPage when no connection
+                    return;
+                }
+                super.onReceivedError(view, errorCode, description, failingUrl);
+            }
+        });
+
+        firstWeb.loadUrl("http://www.event-to-give.com/");
+    }
+
+
+        @Override
+        public boolean onKeyDown ( int keyCode, KeyEvent event){
+            if (event.getAction() == KeyEvent.ACTION_DOWN) {
+                switch (keyCode) {
+                    case KeyEvent.KEYCODE_BACK:
+                        if (firstWeb.canGoBack()) {
+                            firstWeb.goBack();
+                        } else {
+                            finish();
+                        }
+                        return true;
+                }
+
+            }
+            return super.onKeyDown(keyCode, event);
+        }
+
+        @Override
+        public boolean onCreateOptionsMenu (Menu menu)
+        {
+            // Inflate the menu; this adds items to the action bar if it is present.
+            getMenuInflater().inflate(R.menu.main_menu, menu);
+            return true;
+        }
+
+        @Override
+        public boolean onOptionsItemSelected (MenuItem item)
+        {
+            switch (item.getItemId())
+            {
+                case android.R.id.home:
+                    mDrawerLayout.openDrawer(GravityCompat.START);
+                    return true;
+                case R.id.action_facebook:
+                    firstWeb.loadUrl(FACEBOOK_URL);
+                    return true;
+                case R.id.action_twitter:
+                    firstWeb.loadUrl(TWITTER_URL);
+                    return true;
+                default:
+                    return super.onOptionsItemSelected(item);
+            }
+        }
+
+        class DrawerItemClickListener implements ListView.OnItemClickListener {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                selectItem(position);
+            }
+
+            private void selectItem(int position) {
+                switch (position) {
+                    case 0:
+                        loadUrlandClose("http://www.event-to-give.com/");
+                        break;
+                    case 1:
+                        loadUrlandClose("http://www.event-to-give.com/?page_id=8");
+                        break;
+                    case 2:
+                        loadUrlandClose("http://www.event-to-give.com/?page_id=80");
+                        break;
+                    case 3:
+                        loadUrlandClose("http://www.event-to-give.com/?page_id=86");
+                        break;
+                    case 4:
+                        loadUrlandClose("http://www.event-to-give.com/?page_id=2192");
+                        break;
+                    case 5:
+                        loadUrlandClose("http://www.event-to-give.com/?page_id=76");
+                        break;
+                    case 6:
+                        loadUrlandClose("http://www.event-to-give.com/?page_id=364");
+                        break;
+                    case 7:
+                        loadUrlandClose("http://www.event-to-give.com/?page_id=2356");
+                        break;
+                    case 8:
+                        loadUrlandClose("http://www.event-to-give.com/?page_id=88");
+                        break;
+                    case 9:
+                        loadUrlandClose("http://www.event-to-give.com/?page_id=114");
+                        break;
+                    default:
+                }
+            }
+
+            private void loadUrlandClose(String url) {
+                firstWeb.loadUrl(url);
+                mDrawerLayout.closeDrawers();
+            }
+        }
+}
---
+++ app/src/main/java/com/event_to_give/e2g/SplashActivity.java
@@ -0,0 +1,78 @@
+package com.event_to_give.e2g;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.PixelFormat;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+/**
+ * Created by Chrysler on 11/12/2016.
+ * <p>
+ * e2g
+ */
+
+public class SplashActivity extends Activity{
+    public void onAttachedToWindow() {
+        super.onAttachedToWindow();
+        Window window = getWindow();
+        window.setFormat(PixelFormat.RGBA_8888);
+    }
+    /** Called when the activity is first created. */
+    Thread splashTread;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        StartAnimations();
+    }
+    private void StartAnimations() {
+        Animation anim = AnimationUtils.loadAnimation(this, R.anim.alpha);
+        anim.reset();
+        RelativeLayout l=(RelativeLayout) findViewById(R.id.lin_lay);
+        l.clearAnimation();
+        l.startAnimation(anim);
+
+        anim = AnimationUtils.loadAnimation(this, R.anim.translate);
+        anim.reset();
+        TextView iv = (TextView) findViewById(R.id.textView1);
+        iv.clearAnimation();
+        iv.startAnimation(anim);
+        TextView iv2 = (TextView) findViewById(R.id.textView2);
+        iv2.clearAnimation();
+        iv2.startAnimation(anim);
+
+        splashTread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    int waited = 0;
+                    // Splash screen pause time
+                    while (waited < 3500) {
+                        sleep(100);
+                        waited += 100;
+                    }
+                    Intent intent = new Intent(SplashActivity.this,
+                            MainActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                    startActivity(intent);
+                    SplashActivity.this.finish();
+                } catch (InterruptedException e) {
+                    // do nothing
+                } finally {
+                    SplashActivity.this.finish();
+                }
+
+            }
+        };
+        splashTread.start();
+
+    }
+}
